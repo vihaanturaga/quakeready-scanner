8fbbc38188e64690094a1f7669db9962fcce3c24
vihaanturaga <vihaan.turaga@gmail.com>
2026-07-12 19:59:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,23 @@ cocoSsd.load().then(function(model) {
       predictions.forEach(function(prediction) {
         var item = prediction.class;
         var score = Math.round(prediction.score * 100);
+        if (item === 'refrigerator' && score > 50) {
+          found = true;
+          result.innerHTML = '🔴 UNSTABLE: Refrigerator detected! (' + score + '% sure)<br>Fix: Secure with anti-tip strap. Cost: $20-$40';
+          } else if (item === 'tv' && score > 50) {
+          found = true;
+          result.innerHTML = '🔴 UNSTABLE: TV detected! (' + score + '% sure)<br>Fix: Mount to wall with VESA bracket. Cost: $20-$60';
+          } else if (item === 'book' && score > 50) {
+          found = true;
+          result.innerHTML = '🔴 UNSTABLE: Bookcase detected! (' + score + '% sure)<br>Fix: Anchor to wall studs with L-brackets. Cost: $8-$20';
+          } else if (item === 'oven' && score > 50) {
+          found = true;
+          result.innerHTML = '🔴 UNSTABLE: Stove detected! (' + score + '% sure)<br>Fix: Install anti-tip bracket. Cost: $10-$20';
+          } else if (item === 'cabinet' && score > 50) {
+          found = true;
+          result.innerHTML = '🔴 UNSTABLE: Shelving detected! (' + score + '% sure)<br>Fix: Anchor top to wall studs. Cost: $10-$25';
+          }
+        });
+      
+          
         
